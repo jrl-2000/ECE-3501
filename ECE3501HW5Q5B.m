2a68bdae1a00f833a599949d2c505b93f8dc0469
Jonathan Lopez <jonathan.lopez.1224@gmail.com>
2021-09-30 20:46:03 -0400

    Add files via upload

---
+++ ECE3501HW5Q5B.m
@@ -0,0 +1,33 @@
+clc
+
+clear
+
+If = [0 0.5 1.0 1.5 2.0 2.5];
+
+Ea = [0 75 150 205 242 270];
+
+Ra = 0.095;
+
+RI = 2;
+
+Ifield = linspace(0,2.5,100);
+
+for n = 1:100
+
+Ea_curve(n) = spline(If,Ea,Ifield(n));
+
+la(n) = Ea_curve(n)/2.095,
+
+Vt(n) = Ea_curve(n)-la(n)*Ra;
+
+Pload(n) = Ea_curve(n)*la(n)-la(n)^2*Ra;
+
+end
+
+plot(Ifield, Pload)
+
+title ('Field current vs. Load Power')
+
+xlabel('Field current [A]')
+
+ylabel('Load power [W]')
